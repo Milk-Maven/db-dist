e0b5859a582f03c65685e28601b6117b3b1a0a2c
Milk-Maven <tylerf66@gmail.com>
2023-07-24 13:40:48 -0500

    Update port

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
+        }
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("@trpc/server");
 const coupons_1 = require("./routes/coupons");
@@ -36,7 +55,7 @@ const trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 const express_1 = __importDefault(require("express"));
 const prisma = new client_1.PrismaClient();
 const createContext = ({ req, res }) => {
-    return {};
+  return {};
 };
 const t = server_1.initTRPC.context().create();
 const publicProcedure = t.procedure;
@@ -45,13 +64,18 @@ const listingRoutes = (0, listing_1.ListingsRoutes)(prisma, publicProcedure);
 const couponRoutes = (0, coupons_1.CouponRoutes)(prisma, publicProcedure);
 const userRoutes = (0, user_1.UserRoutes)(prisma, publicProcedure);
 const groupRoutes = (0, groups_1.GroupsRoutes)(prisma, publicProcedure);
-const appRouter = router(Object.assign(Object.assign(Object.assign(Object.assign({}, listingRoutes), couponRoutes), userRoutes), groupRoutes));
+const appRouter = router(
+  Object.assign(Object.assign(Object.assign(Object.assign({}, listingRoutes), couponRoutes), userRoutes), groupRoutes)
+);
 const app = (0, express_1.default)();
-app.use("/trpc", trpcExpress.createExpressMiddleware({
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
     router: appRouter,
     createContext,
-}));
-app.listen(3001);
+  })
+);
+app.listen(80);
 // createHTTPServer({
 //   router: appRouter,
 //   createContext(opts: CreateHTTPContextOptions) {
